cf191ce7902321c46b9b7486f2a99b1e91438725
AlessandroDiNardo <alexdna003@gmail.com>
2023-05-04 16:35:47 +0200

    change input type and fix validation

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -10,42 +10,42 @@ namespace la_mia_pizzeria_static.Controllers
                     1, 
                     "Pizza Margherita", 
                     "Best pizza Margherita", 
-                    "https://giorgiacastioni.files.wordpress.com/2015/01/pizza-margherita-1.jpg", 
+                    "img/Margherita.jpg", 
                     6
                 ),
             new Pizza(
                     2, 
                     "Pizza Marinara", 
                     "Best pizza Marinara", 
-                    "https://www.anticogatoleto.com/shop/wp-content/uploads/2021/02/marinara-min-scaled.jpeg", 
+                    "img/Marinara.jpeg", 
                     6
                 ),
             new Pizza(
                     3, 
                     "Pizza Wurstel e patatine", 
                     "Best pizza Wurstel e patatine", 
-                    "https://i0.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2017/11/3244_Pizza.jpg?resize=895%2C616&ssl=1", 
+                    "img/pizzaWurstel.png", 
                     8
                 ),
             new Pizza(
                     4, 
                     "Pizza Mimosa", 
                     "Best pizza Mimosa", 
-                    "https://nizza.ro/wp-content/uploads/2021/02/mimosa.jpg", 
+                    "img/Mimosa.jpg", 
                     8
                 ),
             new Pizza(
                  5, 
                  "Pizza Porchetta", 
                  "Best pizza Porchetta e patatine", 
-                 "https://www.borgotto.it/wp-content/uploads/2020/11/pizza-la-porchetta.jpeg", 
+                 "img/pizza-la-porchetta.jpg", 
                  8
                 ),
             new Pizza(
                     6, 
                     "Pizza Salsicca e friarielli", 
-                    "Best pizza Salsiccia e friarielli", 
-                    "https://www.pizzaontheroad.eu/wp-content/uploads/2020/01/cover-irevolino.jpg", 
+                    "Best pizza Salsiccia e friarielli",
+                    "img/friarielli.jpg", 
                     8
                 ),
 
@@ -70,7 +70,7 @@ namespace la_mia_pizzeria_static.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View("Crate");
+            return View("Create");
         }
 
         [HttpPost]
